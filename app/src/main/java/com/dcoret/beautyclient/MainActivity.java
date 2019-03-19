c48cd87ecb74d625c9c06f909a0a10590f4051e3
HP <hazem.ali1466@gmail.com>
2019-03-19 15:22:07 +0200

    Initial commit

---
+++ app/src/main/java/com/dcoret/beautyclient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dcoret.beautyclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
